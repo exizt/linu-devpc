1e6ee72bb7bf41e7262fd48dc33ae497290829ee
exizt <e2xist@gmail.com>
2022-02-03 03:08:11 +0900

    add utils

--- customize/run.sh
+++ customize/run.sh
@@ -3,6 +3,10 @@
 # bash handling
 if [ -z "$BASH_VERSION" ]; then exec bash "$0" "$@"; exit; fi
 
+# 우분투 저장소 경로를 변경하기
+sudo cp /etc/apt/sources.list /etc/apt/sources.list.bak
+sudo sed -E -i 's#deb http://[a-z.]*archive\.ubuntu\.com/ubuntu#deb https://mirror.kakao.com/ubuntu/#g' /etc/apt/sources.list
+
 # vi에디터로 수정이 잘 안 되니까, vim을 설치
 sudo apt-get install vim
 
---
+++ customize/utils.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+# bash handling
+if [ -z "$BASH_VERSION" ]; then exec bash "$0" "$@"; exit; fi
+
+# meld 설치 - [텍스트 비교 도구]
+# 설명) 윈도우의 winMerge 같은 기능
+# 
+# 설치 후 설정할 것
+# Meld - 설정 - 편집기
+# * Highlight current line : 체크
+# * show line numbers : 체크
+# * use syntax highlighting : 체크
+sudo apt-get install -y meld
+
+# rabbitVCS - [탐색기에 git 아이콘 표시]
+# 설명) 윈도우의 tortoiseGit 같은 기능
+sudo apt update
+sudo apt-get -y install rabbitvcs-nautilus
