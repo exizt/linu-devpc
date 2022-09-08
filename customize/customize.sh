bbac476a7fd3c7d4d94e033d108b0886f830a763
exizt <e2xist@gmail.com>
2022-09-08 20:04:38 +0900

    minor

--- customize/customize.sh
+++ customize/customize.sh
@@ -14,11 +14,16 @@ git config --global user.email e2xist@gmail.com
 git config --global color.ui auto
 
 ## Customize
+# gsettings 설정 하는 것이, sh로 실행될 때에는 오류가 있는 듯하다. 22.04에서 
+# 'dconf-WARNING'이 일어나는 듯. 정확한 원인은 모르겠음. 아마도 'wayland'로 바뀌면서
+# 뭔가가 달라졌나? 커맨드로 실행하면 됨.
 # '빈 화면'이 나타나는 시간. (단위: 초)(설정 - 전원 - 절전 - 빈 화면)
-gsettings set org.gnome.desktop.session idle-delay 1800
+# 30분=1800초
+# gsettings set org.gnome.desktop.session idle-delay 1800
 
 # 빈 화면 활성화 후 화면을 잠그기 전의 시간. (단위: 초)(기본값: 0)
 # (설정 - 개인정보 - 화면 잠금 - 자동 화면 잠금 지연 시간)
-gsettings set org.gnome.desktop.screensaver lock-delay 1800
+# 30분=1800초
+# gsettings set org.gnome.desktop.screensaver lock-delay 1800
 
 
