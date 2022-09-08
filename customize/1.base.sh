395701c0a3b688f23978bf5b056358495f0cc1e0
exizt <e2xist@gmail.com>
2022-09-08 19:25:02 +0900

    bug fix

--- customize/1.base.sh
+++ customize/1.base.sh
@@ -7,7 +7,8 @@ if [ -z "$BASH_VERSION" ]; then exec bash "$0" "$@"; exit; fi
 
 # 우분투 저장소 경로를 변경하기
 sudo cp /etc/apt/sources.list /etc/apt/sources.list.bak
-sudo sed -E -i 's#deb http://[a-z.]*archive\.ubuntu\.com/ubuntu#deb https://mirror.kakao.com/ubuntu/#g' /etc/apt/sources.list
+sudo sed -E -i 's#deb http://[a-z.]*archive\.ubuntu\.com/ubuntu/#deb https://mirror.kakao.com/ubuntu/#g' /etc/apt/sources.list
+sudo apt-get update -y
 
 # vi에디터로 수정이 잘 안 되니까, vim을 설치
 sudo apt-get install -y vim
