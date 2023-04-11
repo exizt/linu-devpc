14fab1c07ccf846cf5a13b9d7b634c02eadd2aff
exizt <e2xist@gmail.com>
2023-04-12 02:36:55 +0900

    minor

--- daemons/install_smartgit.sh
+++ daemons/install_smartgit.sh
@@ -9,6 +9,6 @@
 # bash handling (bash가 아니면 bash로 실행)
 if [ -z "$BASH_VERSION" ]; then exec bash "$0" "$@"; exit; fi
 
-wget -O /tmp/smartgit.deb https://www.syntevo.com/downloads/smartgit/smartgit-21_2_2.deb
+wget -O /tmp/smartgit.deb https://www.syntevo.com/downloads/smartgit/smartgit-22_1_5.deb
 sudo dpkg -i /tmp/smartgit.deb
 sudo rm -f /tmp/smartgit.deb
